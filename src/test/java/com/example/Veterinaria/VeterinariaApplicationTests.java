ec5149d4a513b6523e65eec0493f65df9aa585ae
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-09-05 20:26:54 -0300

    se crea crud de mascotas y duenios

---
+++ src/test/java/com/example/Veterinaria/VeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
